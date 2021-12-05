a60d09888c9888ba1b9b89d2da7d5bc34be5efa8
hritikb27 <hritikb90@gmail.com>
2021-12-06 01:04:29 +0530

    NodeJS Todo App

---
+++ public/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Blog = require("../models/blogSchema");
+
+const dbURI = "mongodb+srv://test-app:testapp123@cluster0.axhrw.mongodb.net/blog-db?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(result => app.listen(3000))
+.catch(err => console.log(err));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs')
+
+app.get('/',(req,res)=>{
+    Blog.find()
+        .then(result=>{
+            console.log(result)
+            res.render('index', {blogs:result});
+        })
+})
+
+app.get('/create-todo', (req,res)=>{
+    res.render('add-todo')
+})
+
+app.post('/add',(req,res)=>{
+    const blog = new Blog(req.body);
+    blog.save()
+        .then(result=>{
+            res.redirect('/');
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+});
+
+app.delete('/blogs/:id', (req,res)=>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+    .then(result=>{
+        res.json({ redirect: '/' })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
